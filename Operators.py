186e9ff3a11b3c1e152e4df949ec086d021eb493
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-10-16 16:54:23 +0530

    Add files via upload

---
+++ Operators.py
@@ -0,0 +1,90 @@
+# 1.Arithmetic Operators
+
+# Addition
+print(3+4)
+print(True + True)
+print(False + False)
+print('Aman'+' Singh')
+
+#Subtraction
+print(4-3)
+print(True - True)
+print(False - False)
+
+#Multiplication
+print(5*6)
+print(True * True)
+print(False * False)
+
+#Division
+print(20/5)
+print(True / True)
+
+
+#Floor Division
+print(30//4)
+print(40//3) # //this is called the floor division
+
+
+#Modulo
+print(5%2)
+print(7%3)
+
+#Exponentiation
+print(3**2)
+print(5**4)
+
+
+#abs
+print(abs(-35))
+print(abs(-2))
+
+#round
+print(round(123.3214567 , 3))
+print(round(7.987657 , 2))
+
+#pow
+print(pow(3,5))
+print(pow(5,3))
+
+# 2.Comparison Operators
+
+# ==
+print(1==2)
+
+# !=
+print(1!=2)
+
+# >
+print(1>2)
+
+# <
+print(1<2)
+
+# <=
+print(1<=2)
+
+# >=
+print(1>=2)
+print('-----------------')
+
+# 3. Logical Operators
+
+#and
+print(True and True)
+print(True and False)
+print(False and False)
+print(False and True)
+print('-----------------')
+
+#or
+print(True or True)
+print(True or False)
+print(False or False)
+print(False or True)
+print('-----------------')
+
+#not
+
+print(not True)
+print(not False)
