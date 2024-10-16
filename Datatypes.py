cc0cc87e097542fe759944fc13fbbd9284e5c174
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-10-16 12:13:11 +0530

    Add files via upload

---
+++ Datatypes.py
@@ -0,0 +1,52 @@
+#INT DATATYPE
+
+print(3)
+print(type(40))
+print(-3)
+print(-199999)
+print('-----------------------')
+
+#FLOAT DATATYPE
+
+print(1.34)
+print(type(-1.34))
+print(-1234566)
+print('-----------------------')
+
+#STRING 
+print('aman')
+print(type('vivek'))
+print("my name is vivek singh")
+print(type('10'))
+print(type('10.44'))
+print(type('true'))
+print("This is vivek's book")
+print('-----------------------')
+
+#BOOLEAN
+
+print(type(True))
+print(False)
+print('-----------------------')
+
+#Functionalities in print statement
+print('a')
+print('b')
+print('c')
+print('a','b','c')
+print('-----------------------')
+
+print('a', end=' ')
+print('b', end=' ')
+print('c')
+
+print('a', end=',')
+print('b', end=',')
+print('c')
+
+print('-----------------------')
+
+print('a','b','c')
+print('a','b','c' ,sep=',')
+print('a','b','c' ,sep='-')
+print('a','b','c' ,sep='----')
