3c4f8651fe4f682b508aed18b5d2bb77ede40c7b
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-10-17 09:57:50 +0530

    Add files via upload

---
+++ Typecasting.py
@@ -0,0 +1,33 @@
+#int
+print(1.878)
+print(int(1.878))
+print('20')
+print(int(True))
+print('------------')
+
+#Float
+print(float(3))
+print(float(True))
+print('------------')
+
+#String 
+print(str(12))
+print(str(12.345))
+print(str(True))
+print('------------')
+
+#bool
+print(bool(1))
+print(bool(100000))
+print(bool(-1211))
+print(bool(0))
+print(bool(0.0))
+print(bool('vivek'))
+print(bool('v'))
+print(bool(' '))
+print(bool(''))  # null string gives the false
+print('------------')
+
+
+#EXCEPTIONS
+print(int(float('3.14')))
