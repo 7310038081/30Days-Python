6b2023e1d7c4e99f164890809507a1f52d425e8a
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-10-18 11:27:24 +0530

    Add files via upload

---
+++ Variables.py
@@ -0,0 +1,19 @@
+a,x,y,z = 10,9,11,13
+print(a,x,y,z)
+
+n = input()
+print(n)
+
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a**b)
+
+#Variables naming conventions
+# 1.Snake case
+# 2.Camel case
+# 3.Pascal case
