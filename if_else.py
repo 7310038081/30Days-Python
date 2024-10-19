7f5f22b34721ff135074c5f8877cfd27cb582aca
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-10-19 22:48:20 +0530

    Add files via upload

---
+++ if_else.py
@@ -0,0 +1,9 @@
+n = int(input('Enter the number: '))    #if else statements
+
+if(n>0):
+    print(n,'the number is +ve')
+elif(n==0):
+    print(n,'the number is zero')
+else:
+    print(n,'the number is -ve')
+
