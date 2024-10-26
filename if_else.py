adaac637b04361d2a32b4afb9059138e8cf62bd5
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-10-26 22:09:59 +0530

    Add files via upload

--- if_else.py
+++ if_else.py
@@ -1,9 +1,25 @@
-n = int(input('Enter the number: '))    #if else statements
+# n = int(input('Enter the number: '))    #if else statements
 
-if(n>0):
-    print(n,'the number is +ve')
-elif(n==0):
-    print(n,'the number is zero')
+# if(n>0):
+#     print(n,'the number is +ve')
+# elif(n==0):
+#     print(n,'the number is zero')
+# else:
+#     print(n,'the number is -ve')
+
+n = float(input("Enter the number: "))
+
+# if(n%5==0):
+#     if(n%3==0):
+#        print("The number is divisible by 5 and 3")
+#     else:
+#         print("The number is not divisble by 3 and 5")
+
+# else:
+#     print("The number is not divisble by 5")
+
+if(n%5==0) and (n%3==0):
+    print("The number is divisible by 5 and 3")
+
 else:
-    print(n,'the number is -ve')
-
+    print("The number is not divisible by 5 and 3")
