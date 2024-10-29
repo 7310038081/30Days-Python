90ef1380a0b80846e8ba88be4019b368614a7920
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-10-29 22:03:53 +0530

    Add files via upload

--- Strings.py
+++ Strings.py
@@ -1,72 +1,103 @@
-# String Concatenation
-print('Vivek' + ' Singh')   #to concatenate the string we use the + opertor btw the string which we want to add
-print('-'*30)
+# name = input("Enter the name: ")
 
-# String Replication
-print("Vivek"*3)       #to replicate the string we multiply the string to n times 
-print('-'*30)
+# #String Slicing
+# string_slicing = name[0:3]
+# print(string_slicing)
+# print('_'*20)
 
-# String Length
-print(len("viveksingh"))  #to find the length of the string we use the length function
-print('-'*30)
-
-# String Slicing
-print("VivekSingh" [2])  
-print("VivekSingh" [-2])
-print("VivekSingh" [-1])
-print("Vivek Singh" [0:5]) #this is string slicing
-print("Vivek Singh" [6:11])
-print("Vivek Singh" [3:])
-print('-'*30)
-
-# String Case Conversion
-print("VIVEKSINGH".lower())  #.lower()
-print("viveksingh".upper())  #.upper()
-print('-'*30)
-
-# String Stripping 
-print("     vivek singh     ")
-print("         vivek singh         " .strip())  #strip function removes the unecessary spaces which are present in th string 
-print('-'*30)
-
-# String Replacing  
-print("Vivek Singh" .replace('i','-'))
-print("Vivek Singh" .replace('Viv','--'))
-print('-'*30)
+# #String slicing with the skip value
+# a = "0123456789"
+# b = print(a[1:7:3])
 
 
-# String Count 
-print("Vivek Singh")
-print("Vivek Singh".count('V'))
-print("Vivek Singh".lower().count('v'))
-print("Vivek Singh".upper().count('V'))
-print('-'*30)
+# c = "abcdefghijklmnopqrstuvwxyz"
+# d = print(c[0:9:4])
+# print('_'*20)
+
+# # String access
+# nameshort1 = name[0] #to access the string through the index
+# nameshort2 = name[1] #to access the string through the index
+# nameshort3 = name[-1] #to access the string through the index
+# nameshort4 = name[-2] #to access the string through the index
+# print(nameshort1 ,nameshort2 , nameshort3 , nameshort4)
+# print('_'*20)
 
 
-#String Find
-print("Vivek Singh".find("ek"))
-print('-'*30)
+# #To find the length of the string we use len function
+# length = len(name)
+# print(length)
 
-# String Check
-print("viveksingh".isalpha())
-print('123'.isalpha())
-print('123'.isdigit())
-print("viveksingh".islower())
-print("VIVEKSINGH".islower())
-print("VIVEKSINGH".isupper())
-print('-'*30)
 
-#String Capitalization
-print("vivek singh".capitalize())
-print("vivek singh".title())
-print('-'*30)
+##############################################################String Functions############################################################
 
-#String startwith or endwith 
-print("Vivek Singh".startswith("Viv"))
-print("Vivek Singh".endswith("snsj"))
-print('-'*30)
+#len("string")
+# full_name = "Vivek Singh"
+# Length = len(full_name)
+# print(Length)
 
-#String adjust center , left , right 
-print("Vivek Singh".center(20,"*"))
-print("Vivek Singh".ljust(20,"*"))
-print("Vivek Singh".rjust(20,"*"))
+
+#.endswith(" ")
+# name = "vivek"
+# print(name.endswith("vk")) 
+# print(name.endswith("vek"))
+
+
+#.startswith(" ")
+# name = "vivek"
+# print(name.startswith("viv"))
+# print(name.startswith("Viv")) # CASE SENSITIVE
+
+#.capitalize("")
+# name = "vivek"
+# print(name.capitalize())
+
+#.title("")
+# name = "vivek singh aman singh"
+# print(name.title())
+
+#.lower()
+# name = "VIVEK"
+# print(name.lower())
+
+#.upper()
+# name = "vivek"
+# print(name.upper())
+
+#.strip()
+# name = "    vivek  "
+# print("my name is",name.strip())
+
+#.split()
+# name = "vivek singh"  # it converts the string into list based on the delimiter 
+# print(name.split())   # by default delimiter is space
+
+#.join()
+# aman = ['vivek','singh']
+# print("_" .join(aman))
+
+#.replace()
+# name = "vivek singh"
+# print(name)
+# print(name.replace("vivek", "Aman"))
+
+#find()
+# name = "vivek"
+# print(name.find("k"))
+
+#isdigit()
+# digit3 = "123455"
+# print(digit3.isdigit())
+# digit1 = str(1234)
+# print(digit1.isdigit())
+
+#isalpha()
+# name = "vivek"
+# print(name.isalpha())
+# digit = "123"
+# print(digit.isalpha())
+
+#isalnum()
+# name = "#"
+# print(name.isalnum())
+# name1 = "vivek123"
+# print(name1.isalnum())
