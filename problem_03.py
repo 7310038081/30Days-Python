b60da195b21430e04a99ac119e67e92c3a79fd46
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-02 10:56:18 +0530

    Add files via upload

---
+++ problem_03.py
@@ -0,0 +1,6 @@
+#check that a tuple cannot changed in a python
+
+tup = (1,2,3,"aman","pikki","kd")
+
+tup[5] = "bu"
+print(tup)
---
+++ problem_04.py
@@ -0,0 +1,4 @@
+#write a program to sum a list with 4 numbers
+l1 = [1,2,3,4,5]
+
+print(sum(l1))
