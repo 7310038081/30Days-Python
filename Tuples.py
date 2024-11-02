b1eee177a5357fc51f98fa413de1ee35e2de27d4
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-03 00:34:32 +0530

    revision of list

---
+++ Tuples.py
@@ -0,0 +1,10 @@
+# Tuples are ordered, immutable collections defined using parentheses. They can contain mixed data types.
+
+# 1.count() Returns the number of occurrences of an item.
+t1 = (1,2,3,3,3,4,5)
+print(t1.count(3))
+
+# 2.index(item) Returns the number of occurrences of an item.
+print(t1.index(4))
+
+
