0412b91cf8ebaf8b8a3c88630642f49a28ac696c
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-03 19:58:36 +0530

    Add files via upload

--- problem_05.py
+++ problem_05.py
@@ -1,4 +1,4 @@
-#write a program to count the number of zeros in the following tuple
-a= (7,0,8,0,0,9)
-
-print(a.count(0))
+s = {}
+print(type(s))
+# what is the type of the 's'?
+# this is dictionary 
