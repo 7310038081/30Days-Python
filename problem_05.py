ed1ef322e65c7183ffd103241a82d149ab698cfb
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-04 11:37:22 +0530

    sets

--- problem_05.py
+++ problem_05.py
@@ -1,4 +1,9 @@
-s = {}
-print(type(s))
-# what is the type of the 's'?
-# this is dictionary 
+#write a program which finds out whether a given username is present in a list or not 
+username = input("Enter the username:")
+
+list1 = ["vivek singh","aman singh","pritika singh","shobhit singh"]
+
+if(username in list1):
+    print("Username present in the list")
+else:
+    print("Username not present in the list")
