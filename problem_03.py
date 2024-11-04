bc7285abaf1b568be97b99d9702e1358e37b4a63
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-04 11:36:33 +0530

    sets

--- problem_03.py
+++ problem_03.py
@@ -1,6 +1,14 @@
-#Can we have a set with 18(int) and '18' (str) as a value in it?
-s = set()
-s.add(18)
-s.add("18")
+# A Spam comment is defined as a text containing following keywords. "Make a lot of money","buy now","subscribe this","click this".Write a 
+# program to detect these spams 
 
-print(s)
+s1 = "Make a lot of money"
+s2 = "buy now"
+s3 = "subscribe this"
+s4 = "click this"
+
+message = input("Enter the message:")
+
+if((s1 in message) or (s2 in message) or (s3 in message) or (s4 in message)):
+    print("This is a spam message:")
+else:
+    print("This is not a spam message:")
