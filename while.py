dd9d4c13710c02c90884c20494bdc5c94c3d1d88
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-06 22:42:58 +0530

    Add files via upload

---
+++ while.py
@@ -0,0 +1,19 @@
+# write a program to print the number from 1 to 50 using while loop
+
+n = 51
+i = 0
+while(i<n):
+    print(i, end=' ')
+    i = i+1
+
+# Write a program to print the content of a list using while loops.
+list1 = ["vivek","singh","shobhit","singh","pritika","singh","rishu","gautam"]
+
+i = 0
+
+
+while(i<len(list1)):
+    print(list1[i],end=" ")
+    i = i+1
+
+
