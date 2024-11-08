59cd1816b312ba70791be2807794ffe116bf4c57
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-08 10:11:53 +0530

    Loops

--- problem_04.py
+++ problem_04.py
@@ -1,7 +1,18 @@
-#write a program to find whether  a given username contains less than 10 characters or not.
-username = input("Enter the username:")
+# Write a program to find whether a given number is prime or not. 
+import math
 
-if(len(username)<10):
-    print("Your username contains less than 10 characters")
+n = int(input("Enter the number: "))
+
+# Step 1: Handle edge cases for numbers less than or equal to 1
+if n <= 1:
+    print("Less than or equal to 1 is not prime")
+elif n == 2:
+    print("2 is a prime number")  # 2 is the only even prime number
 else:
-    print("your username contains more than  10 characters") 
+
+    for i in range(2,n):
+        if n % i == 0:  # If n is divisible by i, it is not prime
+            print(n, "is not a prime number")
+            break
+    else:
+        print(n, "is a prime number")  # If no divisors were found, it's prime
