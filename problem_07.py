f08eca235bc47601568f399abe478be062a425cd
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-08 10:12:34 +0530

    Loops

--- problem_07.py
+++ problem_07.py
@@ -1,8 +1,22 @@
-#  Write a program to find out whether a given post is talking about “Vivek” or not. 
+# Write a program to print the following star pattern. 
+#   * 
+#  *** 
+# ***** for n = 3  
 
-post = input("Enter the sentence here:")
+# n = 3
+# for i in range(1,n+15):
+#     print("*")
+    
+n = int(input("Enter the number:"))
+for i in range(1,n+1):
+    print(" "*(n-i),end='')
+    print("*"*(2*i-1),end="")
+    print("")
 
-if("Vivek".lower() in post.lower()):
-    print("Vivek name is in post")
-else:
-    print("Vivek name is not in post")
+
+
+
+# list1 = ["Data-Science", "Power BI", "Python"]
+
+# for index, subject in enumerate(list1):
+#     print(f"Index: {index}, Subj: {subject}")
