4c00aa73acfae6e5643f970d3694c8bacaed00ef
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-08 10:12:06 +0530

    Loops

--- problem_05.py
+++ problem_05.py
@@ -1,9 +1,16 @@
-#write a program which finds out whether a given username is present in a list or not 
-username = input("Enter the username:")
+# Write a program to find the sum of first n natural numbers using while loop. 
+n = int(input("Enter the number:"))
+sum = 0
 
-list1 = ["vivek singh","aman singh","pritika singh","shobhit singh"]
+i = 0
 
-if(username in list1):
-    print("Username present in the list")
-else:
-    print("Username not present in the list")
+while(i<=n):
+    sum = sum+i
+    i = i+1;
+
+print(sum)
+
+# for i in range (1,n+1):
+#     sum = sum + i
+#     i = i+1
+# print(sum)
