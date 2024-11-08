0d20369ba0f3215f3283ee5be548dac4a552fe02
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-08 11:15:33 +0530

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,50 @@
+#Function Definition
+# def func1():
+#     print("Hello")
+#     print("My name is VIVEK SINGH")
+
+
+# #Function Call
+# func1()
+# func1()
+
+#  Write a program to greet a user with “Good day” using functions.
+
+# username = input()
+
+# def greet():
+#     print(f"Good Day,{username}!")
+
+
+# greet()
+
+
+# def GoodDay(name,ending):
+#     print("Good Day,"+ name)
+#     print(ending)
+
+
+# GoodDay("Vivek","Thank You!")
+
+
+# def GoodDay(name,ending):
+#     print("Good Day,"+ name)
+#     print(ending)
+#     return "okay"
+
+
+# a = GoodDay("Vivek","Thank You!")
+# print(a)
+
+
+# def avg():
+#     a = int(input("Enter the number"))
+#     b = int(input("Enter the number"))
+#     c = int(input("Enter the number"))
+
+#     average = a+b+c/3
+#     return average
+
+# a = avg()
+# print(a)
+
