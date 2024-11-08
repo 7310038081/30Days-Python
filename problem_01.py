f060c1a1526d1b96e0824388d1d49ca4f899f433
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-08 10:10:58 +0530

    Loops

--- problem_01.py
+++ problem_01.py
@@ -1,17 +1,6 @@
-#write a program to find the greatest of four numbers entered by the user . 
+# Write a program to print multiplication table of a given number using for loop. 
 
-a = int(input("Enter the number1:"))
-b = int(input("Enter the number2:"))
-c = int(input("Enter the number3:"))
-d = int(input("Enter the number4:"))
+n = int(input("Enter the number:"))
 
-if(a>b and a>c and a>d):
-    print(a)
-elif(b>a and b>c and b>d):
-    print(b)
-elif(c>a and c>b and c>d):
-    print(c)
-else:
-    print(d)
-
-
+for i in range(1,11):
+    print(f"{n}X{i}={n*i}")
