8ed2ec5dfd377c268c1e8cecde2d004409c6951c
Vivek_052 <85784853+7310038081@users.noreply.github.com>
2024-11-13 12:22:03 +0530

    Add files via upload

---
+++ More_about_Dictionary/dict.py
@@ -0,0 +1,15 @@
+aman = {
+    1:"shobhit",
+    2:"aman",
+    3:"vivek"
+}
+
+print(aman[2])
+
+#methods
+print(aman.items())
+print(aman.keys())
+print(aman.values())
+
+aman.update({1:"bu"})
+print(aman)
